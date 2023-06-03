272d2dae7c5c12d0a6783c54bc9dd6f8de5d89ab
deniskirshenin <deniskirshenin@gmail.com>
2023-06-03 16:47:29 +0400

    adding 11ty and sass to project

---
+++ src/hotels/amara-resort-spa.md
@@ -0,0 +1,24 @@
+---
+title: "Amara Resort & Spa"
+text: "If you’re looking to blend nature’s fiery artistry with a relaxed social vibe, Amara Resort and Spa is whispering your name. Tucked along the banks of Oak Creek Canyon like a secret sanctuary, also just steps from buzzing Uptown Sedona, and close to endless outdoor adventures in the red rock country. Join us for morning yoga, cocktails by the pool, courtyard lawn games and the most glorious sunsets you’ve ever seen. As the perfect four-season destination, Sedona is the ideal getaway for visitors looking to explore the famed red rocks, see the fabled vortexes and enjoy stunning views right from your courtyard Adirondack chair."
+type: "Hotel"
+image: /images/catalog/hotel-amara.jpeg
+fullImage: /images/catalog/amara-hotel-full.jpg
+imageAlt: "Amara Resort & Spa"
+price: "4000"
+rating: "8.5"
+layout: "base.njk"
+---
+
+<section class="hotel-preview">
+  <a class="hotel-preview-button-back" href="/catalog">Back</a>
+  <img class="hotel-preview-image" src="{{fullImage}}" alt="{{imageAlt}}" width="1200" height="540">
+  <h2 class="hotel-preview-title">{{title}}</h2>
+  <div class="hotels-preview-features">
+    <p class="hotel-preview-type">Hotel</p>
+    <p class="hotel-preview-price">From {{price}} ₽</p>
+    <p class="hotel-preview-rating-numbers">Rating: {{rating}}</p>
+  </div>
+  <p class="hotel-preview-text">{{text}}</p>
+  <button class="hotel-preview-button">Order now</button>
+</section>
---
+++ src/js/script.js
@@ -0,0 +1,54 @@
+const wantButton = document.querySelector('.navigation-link-want');
+const findButton = document.querySelector('.find-button');
+const searchButton = document.querySelector('.hotel-search-link');
+const closeModalButton = document.querySelector('.modal-close-button');
+const modalContainer = document.querySelector('.modal-container');
+const subscribeButton = document.querySelector('.form-subscribe-submit-button');
+const subcscriptionContainer = document.querySelector('.subscription-container');
+const subscriptionCloseModalButton = document.querySelector('.subscription-modal-close-button');
+const subscriptionOkButton = document.querySelector('.subscription-ok-button');
+
+wantButton.addEventListener('click', () => {
+  modalContainer.classList.remove('modal-container-close');
+});
+
+findButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  location.href="/catalog";
+});
+
+if (searchButton) {
+  searchButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    modalContainer.classList.remove('modal-container-close');
+  });
+}
+
+closeModalButton.addEventListener('click', () => {
+  modalContainer.classList.add('modal-container-close');
+});
+
+window.addEventListener('click', (evt) => {
+  if (evt.target == modalContainer) {
+    modalContainer.classList.add('modal-container-close');
+  }
+});
+
+subscribeButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  subcscriptionContainer.classList.remove('subscription-container-close');
+});
+
+subscriptionCloseModalButton.addEventListener('click', () => {
+  subcscriptionContainer.classList.add('subscription-container-close');
+});
+
+subscriptionOkButton.addEventListener('click', () => {
+  subcscriptionContainer.classList.add('subscription-container-close');
+});
+
+window.addEventListener('click', (evt) => {
+  if (evt.target == subcscriptionContainer) {
+    subcscriptionContainer.classList.add('subscription-container-close');
+  }
+});
